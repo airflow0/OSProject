d1da9495757d79356b7d5923690028431fda2e44
xeroking <38175762+xeroking@users.noreply.github.com>
2019-03-14 07:03:25 -0400

    Update main.java
    Added variable defs

--- src/KOS/main.java
+++ src/KOS/main.java
@@ -6,13 +6,13 @@ import Handler.*;
 
 public class Main
 {
-    public static Memory memory;
+    /* public static Memory memory;
     public static ProcessControl process;
-    public static Loader load;
+    public static Loader load; */ //Not Needed?
     public static void main(String[] args)
     {
-        process = new ProcessControl();
-        memory = new Memory();
+        Process process = new ProcessControl(); 
+        Memory memory = new Memory(); 
         Loader load = new Loader();
         //LongTerm LongScheduler = new LongTerm();
         for(int i = 0; i < memory.getDiskLength(); i++)
